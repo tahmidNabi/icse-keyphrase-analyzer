e97703795d2c634da60570bc09770ab0bc1ae5cc
Tahmid-un Nabi <sinetob@gmail.com>
2015-12-02 00:57:03 -0800

    Add code for reading from PDF documents

--- src/main/java/com/tnob/Main.java
+++ src/main/java/com/tnob/Main.java
@@ -1,19 +1,19 @@
 package com.tnob;
 
+import com.itextpdf.text.pdf.PdfReader;
+import com.itextpdf.text.pdf.parser.PdfTextExtractor;
 import opennlp.tools.sentdetect.SentenceDetectorME;
 import opennlp.tools.sentdetect.SentenceModel;
 
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 
 /**
  * Hello world!
- *
  */
-public class Main
-{
-    public static void main( String[] args ) throws Exception
-    {
+public class Main {
+    public static void main(String[] args) throws Exception {
         InputStream is = new FileInputStream("models/en-sent.bin");
         SentenceModel model = new SentenceModel(is);
         SentenceDetectorME sdetector = new SentenceDetectorME(model);
@@ -25,5 +25,25 @@ public class Main
         for (String sentence : sentences) {
             System.out.println(sentence);
         }
+
+        String pdfContent = getPDFContent("p1-murphyhill.pdf");
+
+        System.out.println(pdfContent);
+
+
+    }
+
+    public static String getPDFContent(String fileName) throws IOException {
+        PdfReader reader = new PdfReader(fileName);
+
+        int numPages = reader.getNumberOfPages();
+
+        StringBuilder pdfContentBuilder = new StringBuilder();
+
+        for (int page = 1; page <= numPages; page++) {
+            pdfContentBuilder.append(PdfTextExtractor.getTextFromPage(reader, page));
+        }
+
+        return pdfContentBuilder.toString();
     }
 }
